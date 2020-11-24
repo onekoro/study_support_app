fecfd4df3f995bd4e5ab82d755094ca84841cdff
Ubuntu <ubuntu@ip-172-31-39-87.us-east-2.compute.internal>
2020-11-24 09:12:12 +0000

    Finish study_serch

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'study_serch#home'
   get 'study_serch/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
