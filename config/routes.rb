e52ca617d134df4b6435ba1ef8b1334c4ece8de4
Ubuntu <ubuntu@ip-172-31-39-87.us-east-2.compute.internal>
2021-01-08 05:12:47 +0000

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'study_serch#home'
   get 'study_serch/home'
+  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
