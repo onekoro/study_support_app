e725426da363b54f9460ecc71e5b5f720fc5e850
Ubuntu <ubuntu@ip-172-31-39-87.us-east-2.compute.internal>
2021-03-15 06:09:34 +0000

    15-follow-count-error

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,24 +2,43 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
   def create
-    @user = User.find(params[:followed_id])
-    unless current_user.following?(@user)
-      current_user.follow(@user)
+    @target_user = User.find(params[:followed_id])
+    @user = User.find(params[:page_user_id])
+    unless current_user.following?(@target_user)
+      current_user.follow(@target_user)
       respond_to do |format|
-        format.html { redirect_to @user }
+        format.html { redirect_to @target_user }
         format.js
       end
+    # else
+    #   unless current_user.following?(@page_user)
+    #     current_user.follow(@page_user)
+    #     respond_to do |format|
+    #       format.html { redirect_to @user }
+    #       format.js
+    #     end
+    #   end
     end
   end
 
   def destroy
-    @user = Relationship.find(params[:id]).followed
-    if current_user.following?(@user)
-      current_user.unfollow(@user)
+    @target_user = Relationship.find(params[:id]).followed
+    @user = User.find(params[:page_user_id])
+    if current_user.following?(@target_user)
+      current_user.unfollow(@target_user)
       respond_to do |format|
-        format.html { redirect_to @user }
+        format.html { redirect_to @target_user }
         format.js
       end
+    # else
+    #   unless current_user.following?(@page_user)
+    #     current_user.unfollow(@page_user)
+    #     respond_to do |format|
+    #       format.html { redirect_to @user }
+    #       format.js
+    #     end
+    #   end
     end
+    
   end
 end
