4cc7003b4675797bd80a8332b1cab41eb51963fd
Ubuntu <ubuntu@ip-172-31-33-119.us-east-2.compute.internal>
2021-05-22 07:44:11 +0000

    fix-image-upload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -17,9 +17,8 @@ require 'carrierwave/storage/fog'
 
   unless Rails.env.development? || Rails.env.test?
     CarrierWave.configure do |config|
-      config.storage :fog
-      config.fog_provider = 'fog/aws'
-      config.fog_directory  = 'studyplace-app'
+      config.cache_storage = :fog
+      config.fog_directory  = 'study-support'
       config.fog_credentials = {
         provider: 'AWS',
         aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
