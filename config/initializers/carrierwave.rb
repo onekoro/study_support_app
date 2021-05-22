9bd82c4de442de16a00c03dab0bfe324ffd3553b
Ubuntu <ubuntu@ip-172-31-33-119.us-east-2.compute.internal>
2021-05-22 05:19:53 +0000

    webpack-fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -17,14 +17,17 @@ require 'carrierwave/storage/fog'
 
   unless Rails.env.development? || Rails.env.test?
     CarrierWave.configure do |config|
+      config.storage :fog
+      config.fog_provider = 'fog/aws'
+      config.fog_directory  = 'studyplace-app'
       config.fog_credentials = {
         provider: 'AWS',
         aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
         aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-        region: ENV['AWS_DEFAULT_REGION']
+        region: ENV['AWS_DEFAULT_REGION'],
+        path_style: true
       }
-  
-      config.fog_directory  = 'studyplace-app'
-      config.cache_storage = :fog
+      
+      # config.cache_storage = :fog
     end
   end
