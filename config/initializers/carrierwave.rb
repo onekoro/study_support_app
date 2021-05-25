9df1b3d9abf8c3bed0ff6ba63317569e2d2cdc1e
Ubuntu <ubuntu@ip-172-31-33-119.us-east-2.compute.internal>
2021-05-25 06:23:38 +0000

    fix-image-upload-2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,32 +1,38 @@
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
-
-# CarrierWave.configure do |config|
-  # config.storage :fog
-  # config.fog_provider = 'fog/aws'
-  # config.fog_directory  = 'studyplace-app'
-  # config.fog_credentials = {
-  #   provider: 'AWS',
-  #   aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-  #   aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-  #   region: ENV['AWS_DEFAULT_REGION'],
-  #   path_style: true
-  # }
-# end
-
-  unless Rails.env.development? || Rails.env.test?
-    CarrierWave.configure do |config|
-      config.cache_storage = :fog
-      config.fog_directory  = 'study-support'
-      config.fog_credentials = {
-        provider: 'AWS',
-        aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-        region: ENV['AWS_DEFAULT_REGION'],
-        path_style: true
-      }
-      
-      # config.cache_storage = :fog
-    end
+# Rails.env.development? ||
+unless Rails.env.test?
+  CarrierWave.configure do |config|
+    config.cache_storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'study-support'
+    config.fog_public = false
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_DEFAULT_REGION']
+    }
   end
+end
+  
+#   CarrierWave.configure do |config|
+#   if Rails.env.production? || Rails.env.development? # 開発中もs3使う
+#     config.storage :fog
+#     config.fog_provider = 'fog/aws'
+#     config.fog_directory  = 'study-support'
+#     config.asset_host = 'https://s3.amazonaws.com/study-support'
+#     # NOTE: AWS側の設定を変えなくても、この１行の設定でアップロードできた
+#     config.fog_public = false # ←コレ
+#     config.fog_credentials = {
+#       provider: 'AWS',
+#       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+#       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+#       region: ENV['AWS_DEFAULT_REGION'],
+#     }
+#   else
+#     config.storage :file
+#     config.enable_processing = false if Rails.env.test?
+#   end
+# end
